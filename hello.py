d5950a2fd8c4c990742fc8c8230b8482286b4b04
y001003 <y001003@naver.com>
2022-05-09 21:21:33 +0900

    fix : young-soo

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 # 과제: 1부터 10까지 반복하는 과정에서  3의배수일 경우, year를 출력하시오.
 # 나머지 모든 경우는 숫자 그대로 출력
+# 영수가 새로 작업함
 
-
-for i in range(1, 20+1):
+for i in range(1, 20+2):
     if i%15 ==0:
         print('yeardream')
     elif i%3==0 :
